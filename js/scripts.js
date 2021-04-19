4654d924a663655faa22599780733237c45d064b
danielvonboros <dboros40@gmail.com>
2021-04-19 17:40:09 +0200

    updated scripts.js
    removed .push-comments from array pokemonList
    
    initial array as empty array, then added 4 Pokemons to array pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert("Hello World!");
-var favoriteFood = "burger";
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList= [{name: "Pikachu", type: "electric", height: 0.4},
+{name: "Charizard", type:["fire", "flying"], height: 1.7},
+{name: "Pidgeot", type:["flying", "normal"], height: 1.5},
+{name: "Nidoking", type:["ground", "poison"], height: 1.4}];
+
+document.write(pokemonList);
