8f148271beda95ed1502147c4f31729dd0e24dc8
danielvonboros <dboros40@gmail.com>
2021-04-21 10:56:44 +0200

    updated styles.css and scripts.js
    updated files:
    scripts.js and
    styles.css
    
    /js/scripts.js
    document.write
    added unordered list to display pokemon list in html
    added for-loop- and if

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,24 @@ pokemonList= [{name: "Pikachu", type: "electric", height: 0.4},
 {name: "Pidgeot", type:["flying", "normal"], height: 1.5},
 {name: "Nidoking", type:["ground", "poison"], height: 1.4}];
 
-document.write(pokemonList);
+// let text="";
+// for (let i=0; i < pokemonList.length; i++) {
+//   text= text+ " "+pokemonList[i].name+" (height: "+pokemonList[i].height+") "
+//   document.write(text);
+//   i++;
+// };
+
+document.write("<h1>Pokemon List</h1>")
+document.write("<ul>")
+
+let text="";
+let i=0;
+for (;i < pokemonList.length;) {
+  document.write("<li>"+pokemonList[i].name + " (height:" + pokemonList[i].height + ") ");
+  if (pokemonList[i].height > 1.5) {
+    document.write(' - Wow, that\'s big!')
+  }
+  i++;;
+}
+
+document.write("</ul>")
